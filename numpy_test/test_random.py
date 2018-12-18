42b35c3eeb67e5b91982a4808290fbb9f330c4f9
wang yingxin <1196914202@qq.com>
2018-12-18 16:26:03 +0800

    test random

--- numpy_test/test_random.py
+++ numpy_test/test_random.py
@@ -4,3 +4,11 @@ import math
 # set the seed of random
 random.seed(0)
 
+a = random.randrange(0, 10, 4)  # random.randrange(start, stop[, step])
+print(a)
+
+a = random.randint(2, 4)  # random.randint(a, b), Return a random integer N such that a <= N <= b. Alias for randrange(a, b+1).
+print(a)
+
+a = random.random()
+print(a)
