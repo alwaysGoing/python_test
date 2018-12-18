fd0c8a3c023977e04c3f4f0990a04c283b69f4ac
wang yingxin <1196914202@qq.com>
2018-12-18 16:30:53 +0800

    test random

--- numpy_test/test_random.py
+++ numpy_test/test_random.py
@@ -1,5 +1,6 @@
 import random
 import math
+import numpy as np
 
 # set the seed of random
 random.seed(0)
@@ -11,4 +12,19 @@ a = random.randint(2, 4)  # random.randint(a, b), Return a random integer N such
 print(a)
 
 a = random.random()
+print(a)
+
+a = np.random.random(3)
+print(a)
+
+a = np.random.random()
+print(a)
+
+a = np.random.rand(3, 4)  # rand(d0, d1, …, dn), Random values in a given shape.
+print(a)
+
+a = np.random.randn(4, 5)  # randn(d0, d1, …, dn), Return a sample (or samples) from the “standard normal” distribution.
+print(a)
+
+a = np.random.randint(3, 5) # randint(low[, high, size, dtype]), Return random integers from low (inclusive) to high (exclusive).
 print(a)
