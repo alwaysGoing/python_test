665bbf1bd49dc6abc2f5e8dafa764caf4aafd38a
wang yingxin <1196914202@qq.com>
2019-02-27 19:40:36 +0800

    add dict test

---
+++ test_python/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/27 16:30
+# @Author  : Wang
+# @FileName: __init__.py.py
+# @Software: PyCharm
---
+++ test_python/test_dict.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/27 16:30
+# @Author  : Wang
+# @FileName: test_dict.py
+# @Software: PyCharm
